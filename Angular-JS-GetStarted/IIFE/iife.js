e18a4cf854599124053150f31f45a5611549a091
walimi <wahid.alimi@gmail.com>
2016-02-04 08:44:18 -0500

    updated so there are zero global variables. Previous code with var program had one global variable.

--- Angular-JS-GetStarted/IIFE/iife.js
+++ Angular-JS-GetStarted/IIFE/iife.js
@@ -1,31 +1,35 @@
 
 // below is an example of IIFE (immediatly invoked function expression)
 
-var createWorker = function () {
-	
-	// private variable 
-	var workCount = 0;
-	
-	// private implementaion details
-	var task1 = function () {
-		workCount += 1;
-		console.log("task1 " + workCount);
-	};
-	
-	var task2 = function () {
-		workCount += 1; 
-		console.log("task2 " + workCount);
-	};
-	
-	// what's exposed to external clients
-	return {
-		job1: task1,
-		job2: task2
-	};
-};
+function () {
 
-// client
-var worker = createWorker();
+	var createWorker = function () {
+		
+		// private variable 
+		var workCount = 0;
+		
+		// private implementaion details
+		var task1 = function () {
+			workCount += 1;
+			console.log("task1 " + workCount);
+		};
+		
+		var task2 = function () {
+			workCount += 1; 
+			console.log("task2 " + workCount);
+		};
+		
+		// what's exposed to external clients
+		return {
+			job1: task1,
+			job2: task2
+		};
+	};
+	
+	// client
+	var worker = createWorker();
+	
+	worker.job1();
+	worker.job2();
 
-worker.job1();
-worker.job2();
+}(); 
