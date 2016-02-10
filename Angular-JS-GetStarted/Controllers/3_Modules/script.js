2723b6a01eaf7ba84af4d0354eb8677c33889079
walimi <wahid.alimi@gmail.com>
2016-02-10 08:29:29 -0500

    Added module code.

--- Angular-JS-GetStarted/Controllers/3_Modules/script.js
+++ Angular-JS-GetStarted/Controllers/3_Modules/script.js
@@ -1,18 +1,24 @@
 (function(){
+    
+    var app = angular.module("githubViewer", []); 
+    
     var MainController = function($scope, $http) {
 
         var onUserComplete = function(response) {
             $scope.user = response.data;
-
+            $scope.error = "";    
         };
 
         var onError = function (reason) {}
             $scope.error = "Could not fetch the user.";
         ;
 
-        $http.get("https://ap.github.com/users/walimi")
+        $http.get("https://api.github.com/users/robconery")
              .then(onUserComplete, onError);
 
         $scope.message = "Hello, Angular!"; 
     };
+    
+    app.controller("MainController", MainController); 
+    
 }());
