484d0cd413e9e05340ac93577fa8c2d4972a614f
walimi <wahid.alimi@gmail.com>
2016-02-19 08:12:03 -0500

    Added EventDetails.html and EventController.js files.

---
+++ Angular-Fundamentals/DemoApp/app/js/controllers/EventController.js
@@ -0,0 +1,12 @@
+'use strict'; 
+
+eventsApp.controller('EventController', 
+	function EventController($scope) {
+		
+		$scope.event = {
+			name: 'Angular Boot Camp',
+			date: '1/1/2013',
+			time: '10:30am'	
+		}
+	}	
+);
