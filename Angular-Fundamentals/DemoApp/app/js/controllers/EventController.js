97b1f7fba00b42a6a6035192d3d8708bcd838990
walimi <wahid.alimi@gmail.com>
2016-02-19 08:45:37 -0500

    Added session information using ng-repeat. Added additional session information, (i.e., creatorname, duration, level)

--- Angular-Fundamentals/DemoApp/app/js/controllers/EventController.js
+++ Angular-Fundamentals/DemoApp/app/js/controllers/EventController.js
@@ -12,7 +12,31 @@ eventsApp.controller('EventController',
 				city: 'Mountain View',
 				state: 'CA'
 			},
-			imageUrl: '/img/angularjs-logo.png'	
+			imageUrl: '/img/angularjs-logo.png',
+			sessions: [
+				{ 
+					name: 'Directives Masterclass',
+					creatorName: 'Bob Smith',
+					duration: '1 hr',
+					level: 'Advanced',
+				 	abstract: 'In this session you will learn ins and outs of directives!'
+				},
+				{ 
+					name: 'Scopes for fun and profit',
+					creatorName: 'John Doe',
+					duration: '30 mins',
+					level: 'Introductory',
+				 	abstract: 'This sesssion will take a closer look at the scopes. Learn what they do, how they do it, and how to get them to do it for you.'
+				},
+				{ 
+					name: 'Well behaved Controllers', 
+					creatorName: 'Jane Doe',
+					duration: '2 hours',
+					level: 'Intermediate',
+				 	abstract: 'Controllers are the beginning of everything Angular related. Learn how to craft controllers that will win the respect of your friends and neighbors.'
+				}
+				
+			]	
 		}
 	}	
 );
