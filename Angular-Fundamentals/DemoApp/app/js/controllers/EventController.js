d82e45111faffee3da238b1bab61d0e08845af47
walimi <wahid.alimi@gmail.com>
2016-02-19 08:23:00 -0500

    Added location information to the page using bootstrap styling.

--- Angular-Fundamentals/DemoApp/app/js/controllers/EventController.js
+++ Angular-Fundamentals/DemoApp/app/js/controllers/EventController.js
@@ -6,7 +6,12 @@ eventsApp.controller('EventController',
 		$scope.event = {
 			name: 'Angular Boot Camp',
 			date: '1/1/2013',
-			time: '10:30am'	
+			time: '10:30am',
+			location: {
+				address: 'Google Headquarters',
+				city: 'Mountain View',
+				state: 'CA'
+			}	
 		}
 	}	
 );
