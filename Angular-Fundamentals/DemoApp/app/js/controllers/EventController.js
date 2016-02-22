71e942a52843066cb05f0ac58835ba9e5973481c
walimi <wahid.alimi@gmail.com>
2016-02-22 08:29:25 -0500

    Other Directives: Part 1. Usage of ng-bind-template to sanitize html.

--- Angular-Fundamentals/DemoApp/app/js/controllers/EventController.js
+++ Angular-Fundamentals/DemoApp/app/js/controllers/EventController.js
@@ -3,6 +3,8 @@
 eventsApp.controller('EventController', 
 	function EventController($scope) {
 		
+		//$scope.snippet = '<span style="color:red">hi there!</span>'; 
+		
 		$scope.event = {
 			name: 'Angular Boot Camp',
 			date: '1/1/2013',
