fa0c68d4a2ea8489d3b688a87f224ed365cbdbd1
walimi <wahid.alimi@gmail.com>
2016-02-27 08:08:06 -0500

    Updated EditEventController to use .then and .catch. Still getting a 404 when trying to save. I posted a question in discussion for the course.

--- Angular-Fundamentals/DemoApp/app/js/controllers/EditEventController.js
+++ Angular-Fundamentals/DemoApp/app/js/controllers/EditEventController.js
@@ -5,10 +5,9 @@ eventsApp.controller('EditEventController',
         $scope.saveEvent = function(event, newEventForm) {
             if(newEventForm.$valid) {
                 eventDataService.save(event)
-                    .$promise.then(
-                        function(response) { console.log('success', response); },
-                        function(response) { console.log('failure', response); }
-                    );
+                    .$promise
+                    .then(function(response) { console.log('success', response); })
+                    .catch(function(response) { console.log('failure', response); });
             }
         };
 
--- Angular-Fundamentals/DemoApp/app/js/services/EventDataService.js
+++ Angular-Fundamentals/DemoApp/app/js/services/EventDataService.js
@@ -7,7 +7,7 @@ eventsApp.factory('eventDataService', function($resource) {
             return resource.get({id:1});
         },
         save: function(event) {
-            event.id = 9; // this is just for demo. in real life you'd have to grab the max id and add 1 to it.
+            event.id = 9; // this is just for demo. in real world app you'd have to grab the max id and add 1 to it.
             return resource.save(event);
         }
     };
