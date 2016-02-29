afd3832690883a7fecea8e39fd3bfb9628f5586d
walimi <wahid.alimi@gmail.com>
2016-02-29 07:41:49 -0500

    Using  service. Second example using context.

--- Angular-Fundamentals/DemoApp/app/js/controllers/CompileSampleController.js
+++ Angular-Fundamentals/DemoApp/app/js/controllers/CompileSampleController.js
@@ -10,6 +10,14 @@ eventsApp.controller('CompileSampleController',
         var fn = $parse('1 + 2');
         console.log(fn());
 
+        var getter = $parse('event.name'); // getter is the name of the function
+
+        var context1 = {event: {name: 'AngularJS Boot Camp'}};
+        var context2 = {event: {name: 'Code Camp'}};
+
+        console.log(getter(context1));
+        console.log(getter(context2));
+
         $scope.appendDivToElement = function (markup) {
             return $compile(markup) ($scope).appendTo(angular.element("#appendHere"));
         };
