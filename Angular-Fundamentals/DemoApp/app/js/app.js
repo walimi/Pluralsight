0517c0610c8ef7d09d65caa3ec13b67db2d958a1
walimi <wahid.alimi@gmail.com>
2016-03-02 08:21:52 -0500

    Added HTML5 routing which means getting rid of the hash (#) from the route urls.

--- Angular-Fundamentals/DemoApp/app/js/app.js
+++ Angular-Fundamentals/DemoApp/app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
-    .config(function($routeProvider){
+    .config(function($routeProvider, $locationProvider){
         $routeProvider.when('/newEvent',
             {
                 templateUrl: 'templates/NewEvent.html',
@@ -19,4 +19,5 @@ var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
                 controller: 'EventController'
             });
         $routeProvider.otherwise({redirectTo: '/events'});
+        $locationProvider.html5Mode(true);
     });
