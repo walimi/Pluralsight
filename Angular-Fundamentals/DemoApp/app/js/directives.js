64fa2b6fef54ce2320fbde92207cf8c3b411d40a
walimi <wahid.alimi@gmail.com>
2016-03-03 08:38:27 -0500

    Using the template property to specify the HTML. By using the template property, Angular automatically compiles the template so you don't have to do it explicitly like you did before.

--- Angular-Fundamentals/DemoApp/app/js/directives.js
+++ Angular-Fundamentals/DemoApp/app/js/directives.js
@@ -3,10 +3,6 @@
 eventsApp.directive('mySample', function($compile){
     return {
         restrict: 'E',
-        link: function(scope, element, attrs, controller) {
-            var markup = "<input type='text' ng-model='sampleData' /> {{sampleData}} <br />";
-            angular.element(element).html($compile(markup)(scope));
-        }
+        template: "<input type='text' ng-model='sampleData' /> {{sampleData}} <br />"
     };
-
 });
