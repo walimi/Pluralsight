3722e03537f998aa454a219f330924243bd659a0
walimi <wahid.alimi@gmail.com>
2016-03-03 08:35:09 -0500

    Using the custom directive from previous commit as HTML element instead of an HTML attribute.

--- Angular-Fundamentals/DemoApp/app/js/directives.js
+++ Angular-Fundamentals/DemoApp/app/js/directives.js
@@ -2,6 +2,7 @@
 
 eventsApp.directive('mySample', function($compile){
     return {
+        restrict: 'E',
         link: function(scope, element, attrs, controller) {
             var markup = "<input type='text' ng-model='sampleData' /> {{sampleData}} <br />";
             angular.element(element).html($compile(markup)(scope));
