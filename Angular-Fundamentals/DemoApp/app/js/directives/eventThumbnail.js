cf965ee303645ea499967d56439fbb4127dc9bb8
walimi <wahid.alimi@gmail.com>
2016-03-05 10:37:27 -0500

    Set the replace property of the directive to true. This replaces element event-thumbnail which is not a valid HTML element with an anchor (i.e., a) elelement.

--- Angular-Fundamentals/DemoApp/app/js/directives/eventThumbnail.js
+++ Angular-Fundamentals/DemoApp/app/js/directives/eventThumbnail.js
@@ -6,6 +6,7 @@
 eventsApp.directive('eventThumbnail', function() {
     return {
         restrict: 'E',
+        replace: true,
         templateUrl: '/templates/directives/eventThumbnail.html'
     }
 });
