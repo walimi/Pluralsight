b44b193697bf6b920ce20e81d143f98b536a033c
walimi <wahid.alimi@gmail.com>
2016-03-05 10:35:07 -0500

    Created an event-thumbnail custom element directive.

---
+++ Angular-Fundamentals/DemoApp/app/js/directives/eventThumbnail.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Wahid on 3/5/2016.
+ */
+'use strict';
+
+eventsApp.directive('eventThumbnail', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '/templates/directives/eventThumbnail.html'
+    }
+});
