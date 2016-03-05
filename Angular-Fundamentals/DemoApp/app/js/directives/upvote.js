23d9d75818648b218ab7a9fc69417ca7ed013ce6
walimi <wahid.alimi@gmail.com>
2016-03-05 11:05:25 -0500

    Created a custom directive for upvote functionality.

---
+++ Angular-Fundamentals/DemoApp/app/js/directives/upvote.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Wahid on 3/5/2016.
+ */
+'use strict';
+
+eventsApp.directive('upvote', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '/templates/directives/upvote.html',
+        scope: {
+            upvote: "&", // ampersand means that we're going to pass a function and execute that function in the parent scope
+            downvote: "&",
+            count: "="  // equal sign means we're going to pass an object
+        }
+    }
+});
