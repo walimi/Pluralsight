32780b9b0b1c988bd1ca507a2b836d7d798a9112
walimi <wahid.alimi@gmail.com>
2016-03-08 08:51:50 -0500

    Understanding transclusion. Updated the EventDetail.html to toggle visibility of the sessions when the user clicks  on the header. Created a new directive named collabsible.

---
+++ Angular-Fundamentals/DemoApp/app/js/directives/collapsible.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Wahid on 3/8/2016.
+ */
+'use strict';
+
+eventsApp.directive('collapsible', function(){
+   return {
+       restrict: 'E',
+       replace: true,
+       template: '<div><h4 class="well-title" ng-click="toggleVisibility()">{{title}}</h4><div ng-show="visible" ng-transclude></div></div>',
+       transclude: true,
+       controller: function($scope) {
+           $scope.visible = true;
+           $scope.toggleVisibility = function() {
+               $scope.visible = !$scope.visible;
+           };
+       },
+       scope: {
+           title: '@'
+       }
+   }
+});
