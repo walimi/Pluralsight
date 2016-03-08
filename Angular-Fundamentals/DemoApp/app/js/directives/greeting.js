3f1bef78edad610653471800144d354161cdc03e
walimi <wahid.alimi@gmail.com>
2016-03-08 08:15:20 -0500

    Sharing directive controlllers via require. Added functionality which shares a directive controller across different custom directives.

--- Angular-Fundamentals/DemoApp/app/js/directives/greeting.js
+++ Angular-Fundamentals/DemoApp/app/js/directives/greeting.js
@@ -1,20 +1,39 @@
 /**
  * Created by Wahid on 3/8/2016.
  */
-eventsApp.directive('greeting', function(){
-   return {
-       restrict: 'E',
-       replace: true,
-       template: "<button class='btn' ng-click='sayHello()'>Say Hello</button>",
-       controller: '@',
-       name: 'ctrl'
-   };
-});
-
-
-eventsApp.controller('GreetingController',
-    function GreetingController($scope){
-        $scope.sayHello = function() {
-            alert('Hello');
+eventsApp
+    .directive('greeting', function(){
+       return {
+           restrict: 'E',
+           replace: true,
+           template: "<button class='btn' ng-click='sayHello()'>Say Hello</button>",
+           controller: function GreetingController($scope){
+               var greetings = ['hello'];
+               $scope.sayHello = function() {
+                   alert(greetings.join());
+               }
+               this.addGreeting = function(greeting) {
+                   greetings.push(greeting);
+               }
+           }
+       }
+    })
+    .directive('finnish', function() {
+        return {
+            restrict: 'A',
+            require: 'greeting',
+            link: function(scope, element, attrs, controller) {
+                controller.addGreeting('hei');
+            }
+        }
+    })
+    .directive('turkish', function() {
+        return {
+            restrict: 'A',
+            require: 'greeting',
+            link: function(scope, element, attrs, controller) {
+                controller.addGreeting('merhaba');
+            }
         }
     });
+
