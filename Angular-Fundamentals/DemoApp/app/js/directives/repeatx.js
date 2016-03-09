09153a917352efd0ae714a811b2a2dd9118f0e25
walimi <wahid.alimi@gmail.com>
2016-03-09 07:33:12 -0500

    Using Compile to Transform the DOM. Using the link property with complex HTML.

--- Angular-Fundamentals/DemoApp/app/js/directives/repeatx.js
+++ Angular-Fundamentals/DemoApp/app/js/directives/repeatx.js
@@ -1,11 +1,11 @@
 /**
  * Created by Wahid on 3/9/2016.
  */
-eventsApp.directive('repeatX', function() {
+eventsApp.directive('repeatX', function($compile) {
     return {
         link: function(scope, element, attributes, controller) {
             for (var i=0; i < Number(attributes.repeatX)-1; i++) {
-                element.after(element.clone().attr('repeat-x', 0));
+                element.after($compile(element.clone().attr('repeat-x', 0))(scope));
             }
         }
     };
