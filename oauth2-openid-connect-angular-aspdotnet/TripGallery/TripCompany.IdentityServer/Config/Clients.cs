9d9a702ec56b701462f73c0134f386f2a8b0491b
aalimi <wahid.alimi@frontpointsecurity.com>
2016-03-29 18:12:36 -0400

    client credentials flow. added code to the mvc application for token based authorization

--- oauth2-openid-connect-angular-aspdotnet/TripGallery/TripCompany.IdentityServer/Config/Clients.cs
+++ oauth2-openid-connect-angular-aspdotnet/TripGallery/TripCompany.IdentityServer/Config/Clients.cs
@@ -10,7 +10,24 @@ namespace TripCompany.IdentityServer.Config
     {
         public static IEnumerable<Client> Get()
         {
-            return new List<Client>(); 
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "tripgalleryclientcredentials",
+                    ClientName = "Trip Gallery (Client Credentials)",
+                    Flow = Flows.ClientCredentials, 
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret(TripGallery.Constants.TripGalleryClientSecret.Sha256())
+                    },
+                    AllowAccessToAllScopes = true//,
+                    //AllowedScopes = new List<string>()
+                    //{
+                    //    "gallerymanagement"
+                    //}
+                }
+            };
         }
     }
 }
--- oauth2-openid-connect-angular-aspdotnet/TripGallery/TripGallery.MVCClient/Helpers/TripGalleryHttpClient.cs
+++ oauth2-openid-connect-angular-aspdotnet/TripGallery/TripGallery.MVCClient/Helpers/TripGalleryHttpClient.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web;
+using IdentityModel.Client;
 
 namespace TripGallery.MVCClient.Helpers
 {
@@ -14,17 +10,49 @@ namespace TripGallery.MVCClient.Helpers
     {
 
         public static HttpClient GetClient()
-        { 
-            HttpClient client = new HttpClient(); 
+        {
            
-            client.BaseAddress = new Uri(Constants.TripGalleryAPI);
 
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(Constants.TripGalleryAPI);
             client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            // client credentials flow
+            var accessToken = RequestAccessTokenClientCredentials();
+            client.SetBearerToken(accessToken);
 
             return client;
         }
- 
+
+        private static string RequestAccessTokenClientCredentials()
+        {
+            // did we store the token before?
+           var cookie = HttpContext.Current.Request.Cookies.Get("TripGalleryCookie");
+            if (cookie?["access_token"] != null) //if(cookie != null && cookie["access_token"] != null)
+            {
+                return cookie["access_token"]; 
+            }
+
+
+            // create a Token Client
+            var tokenClient = new TokenClient(TripGallery.Constants.TripGallerySTSTokenEndpoint,
+                                            "tripgalleryclientcredentials",
+                                            TripGallery.Constants.TripGalleryClientSecret);
+
+            // ask for a token, containing the gallerymanegement scope
+            var tokenResponse = tokenClient.RequestClientCredentialsAsync("gallerymanagement").Result;
+            
+            // decode & write out the token, so we can see what's in it
+            TokenHelper.DecodeAndWrite(tokenResponse.AccessToken);
+
+            // we save the token in a cookie for use later on
+            HttpContext.Current.Response.Cookies["TripGalleryCoookie"]["access_token"] = tokenResponse.AccessToken;
+
+            return tokenResponse.AccessToken; 
+
+        }
+
     }
 }
