6d53c139d37b87cbcce5234414ac4fb576e30d4a
aalimi <wahid.alimi@frontpointsecurity.com>
2016-03-31 18:31:28 -0400

    initial commit. simple hello world pipeline.

---
+++ OWIN_and_Katana/Pluralsight.Owin.Demo/Pluralsight.Owin.Demo/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Web;
+using Owin;
+
+namespace Pluralsight.Owin.Demo
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                await ctx.Response.WriteAsync("Hello world");
+            });
+        }
+
+    }
+}
