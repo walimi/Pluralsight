22398731359cb8033d428d15fa3a03204c85978b
aalimi <wahid.alimi@frontpointsecurity.com>
2016-03-31 18:41:44 -0400

    added another owin pipeline.

--- OWIN_and_Katana/Pluralsight.Owin.Demo/Pluralsight.Owin.Demo/Startup.cs
+++ OWIN_and_Katana/Pluralsight.Owin.Demo/Pluralsight.Owin.Demo/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using System.Web;
@@ -11,9 +12,17 @@ namespace Pluralsight.Owin.Demo
     {
         public static void Configuration(IAppBuilder app)
         {
+
             app.Use(async (ctx, next) =>
             {
-                await ctx.Response.WriteAsync("Hello world");
+                Debug.WriteLine("Incoming request: " + ctx.Request.Path);
+                await next();
+                Debug.WriteLine("Outgoing request: " + ctx.Request.Path);
+            });
+
+            app.Use(async (ctx, next) =>
+            {
+                await ctx.Response.WriteAsync("<html><head></head><body>Hello world</body></html>");
             });
         }
 
