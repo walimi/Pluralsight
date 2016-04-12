7c0ab3e75d77f89328240c3392b59d798d69f06c
walimi <wahid.alimi@gmail.com>
2016-04-12 08:33:24 -0400

    refactored code to private method to execute tests. (part of DRY principal.)

--- OWIN_and_Katana/Pluralsight.Owin.Demo.Hosting/Pluralsight.Owin.Demo.Tests/OwinTests.cs
+++ OWIN_and_Katana/Pluralsight.Owin.Demo.Hosting/Pluralsight.Owin.Demo.Tests/OwinTests.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.Owin.Testing;
 using Pluralsight.Owin.Demo.Hosting;
 using System.Net;
+using System.Net.Http;
+using System;
 
 namespace Pluralsight.Owin.Demo.Tests
 {
@@ -12,23 +14,22 @@ namespace Pluralsight.Owin.Demo.Tests
         [TestMethod]
         public async Task Owin_returns_200_on_request_to_root()
         {
-            using (var server = TestServer.Create<Startup>())
-            {
-                var response = await server.HttpClient.GetAsync("/");
-                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-            }
+            var statusCode = await CallServer(async x => {
+                var response = await x.GetAsync("/");
+                return response.StatusCode;
+            });
+            Assert.AreEqual(HttpStatusCode.OK, statusCode);
         }
 
 
         [TestMethod]
         public async Task Owin_returns_hello_world_on_request_to_root()
         {
-            using (var server = TestServer.Create<Startup>())
-            {
-                var response = await server.HttpClient.GetAsync("/");
-                var body = await response.Content.ReadAsStringAsync();
-                Assert.AreEqual("Hello World", body);
-            }
+            var body = await CallServer(async x => {
+                var response = await x.GetAsync("/");
+                return await response.Content.ReadAsStringAsync();
+            });
+            Assert.AreEqual("Hello World", body);
         }
 
 
@@ -36,15 +37,20 @@ namespace Pluralsight.Owin.Demo.Tests
         [TestMethod]
         public async Task Owin_returns_correct_contenttype_on_request_to_jpg()
         {
-            using (var server = TestServer.Create<Startup>())
-            {
-                var response = await server.HttpClient.GetAsync("/rayan-taekwondo.jpg");
-                var contentType = response.Content.Headers.ContentType.MediaType;
-                Assert.AreEqual("image/jpeg", contentType);
-            }
+            var contentType = await CallServer(async x => {
+                var response = await x.GetAsync("/rayan-taekwondo.jpg");
+                return response.Content.Headers.ContentType.MediaType;
+            });
+            Assert.AreEqual("image/jpeg", contentType);
         }
 
 
-
+        private async Task<T> CallServer<T>(Func<HttpClient, Task<T>> callback)
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                return await callback(server.HttpClient);
+            }
+        }
     }
 }
