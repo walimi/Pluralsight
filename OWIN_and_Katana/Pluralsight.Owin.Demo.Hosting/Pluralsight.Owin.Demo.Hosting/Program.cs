98375c4ad6154cd4e42082419599019a27e81680
walimi <wahid.alimi@gmail.com>
2016-04-12 07:47:39 -0400

    created a self-hosted OWIN pipeline that responds to HTTP requests.

---
+++ OWIN_and_Katana/Pluralsight.Owin.Demo.Hosting/Pluralsight.Owin.Demo.Hosting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace Pluralsight.Owin.Demo.Hosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:12345"))
+            {
+                Console.WriteLine("Listeningn on port 12345");
+                Console.WriteLine("Press Enter to end...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ OWIN_and_Katana/Pluralsight.Owin.Demo.Hosting/Pluralsight.Owin.Demo.Hosting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Owin;
+
+namespace Pluralsight.Owin.Demo.Hosting
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            app.Use(async (ctx, next) => {
+                await ctx.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
