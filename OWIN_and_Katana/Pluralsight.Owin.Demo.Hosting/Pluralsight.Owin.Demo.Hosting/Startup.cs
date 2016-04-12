7d2960953d1bddef087a79b19da6c29193a875bc
walimi <wahid.alimi@gmail.com>
2016-04-12 07:57:38 -0400

    added code to serve static files from self-hosted OWIN pipeline.

--- OWIN_and_Katana/Pluralsight.Owin.Demo.Hosting/Pluralsight.Owin.Demo.Hosting/Startup.cs
+++ OWIN_and_Katana/Pluralsight.Owin.Demo.Hosting/Pluralsight.Owin.Demo.Hosting/Startup.cs
@@ -6,6 +6,7 @@ namespace Pluralsight.Owin.Demo.Hosting
     {
         public static void Configuration(IAppBuilder app)
         {
+            app.UseStaticFiles();
             app.Use(async (ctx, next) => {
                 await ctx.Response.WriteAsync("Hello world");
             });
