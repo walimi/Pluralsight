80d5bf29961c68dcb78133c9607a6ab5d337811a
walimi <wahid.alimi@gmail.com>
2016-05-05 08:20:25 -0400

    created loaders and preloaders.

--- Webpack_Fundamentals/webpack.config.js
+++ Webpack_Fundamentals/webpack.config.js
@@ -3,5 +3,25 @@ module.exports = {
 	output: {
 		filename: "bundle.js"
 	}, 
-	watch: true
+	module: {
+		preloaders:[
+			{
+				test:/\.js$/,
+				exclude: 'node_modules',
+				loader: 'jshint-loader'
+			}
+		],
+		loaders: [
+			{
+				test: /\.es6$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			}
+		]
+	},
+
+	resolve: {
+		extensions: ['', '.js', '.es6']
+	}
+	
 }
