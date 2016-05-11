2176132ae8a25e4e79afe3f0c7bd7079fd7e2e02
walimi <wahid.alimi@gmail.com>
2016-05-11 08:34:24 -0400

    Added unit test for save function in userDatajs file.

--- Angular-Fundamentals/DemoApp/test/unit/userDataSpec.js
+++ Angular-Fundamentals/DemoApp/test/unit/userDataSpec.js
@@ -9,7 +9,8 @@ describe('userData', function(){
     beforeEach(module('eventsApp'));
 
     beforeEach(function(){
-        mockUserResource = sinon.stub({get: function(){}});
+        // Creating mock userResource object and the methods associated with the real object. 
+        mockUserResource = sinon.stub({get: function(){}, save: function(){}});
 
         // In the following line of code we're explicitly telling Angular to return a mockUserResource
         // object whenever someone asks for userResource service.
@@ -20,10 +21,22 @@ describe('userData', function(){
     });
 
     describe('getUser', function() {
-      it('should call getresource.get with the username', inject(function(userData){
-         userData.getUser('bob');
+        it('should call userResource.get with the username', inject(function(userData){
+            userData.getUser('bob');
+            expect(mockUserResource.get.args[0][0]).toEqual({userName: 'bob'});
+        }));
 
-          expect(mockUserResource.get.args[0][0]).toEqual({userName: 'bob'});
-      }))
+        it('should return whatever userResource.get returns', inject(function(userData){
+            mockUserResource.get.returns('angular rocks');
+            var result = userData.getUser('bob');
+            expect(result).toBe('angular rocks');
+        }));
+    });
+
+    describe('save', function(){
+        it('should call userResource.save with the same parameter', inject(function(userData){
+            userData.save('some value');
+            expect(mockUserResource.save.calledWith('some value')).toBe(true);
+        }));
     });
 });
