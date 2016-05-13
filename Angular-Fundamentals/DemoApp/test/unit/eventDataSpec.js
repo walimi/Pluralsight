57799f2ede1689bce92d149ce39e18b39618a5ff
walimi <wahid.alimi@gmail.com>
2016-05-13 08:36:25 -0400

    Testing AJAX services Part 2. Added more tests to eventDataSpec.js.

--- Angular-Fundamentals/DemoApp/test/unit/eventDataSpec.js
+++ Angular-Fundamentals/DemoApp/test/unit/eventDataSpec.js
@@ -20,4 +20,35 @@ describe('eventData', function(){
             $httpBackend.verifyNoOutstandingRequest();
         })
     );
+
+    it('should return the correct data when getEvent is called',
+        inject(function(eventData, $httpBackend){
+            $httpBackend.expect('GET', '/data/event/11').respond({name: 'My Event'});
+            var event = eventData.getEvent(11);
+            $httpBackend.flush();
+
+            expect(event.name).toBe('My Event');
+        })
+    );
+
+    it('it should set the id to 999 when save is called',
+        inject(function(eventData, $httpBackend) {
+            $httpBackend.expect('POST', '/data/event/999').respond({});
+            var event = { name: 'My Event'};
+            eventData.save(event);
+            $httpBackend.flush();
+
+            expect(event.id).toBe(999);
+        })
+    );
+
+    it('should issue a GET request to data/event when getAllEvents is called',
+        inject(function(eventData, $httpBackend){
+            $httpBackend.when('GET', '/data/event').respond([{name: 'My Event'}]);
+            var events = eventData.getAllEvents();
+            $httpBackend.flush();
+
+            expect(events[0].name).toBe('My Event');
+        })
+    );
 });
