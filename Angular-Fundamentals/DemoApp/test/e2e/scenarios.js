cd64c710f640cddc8f4dca07e25751ab4aab436d
walimi <wahid.alimi@gmail.com>
2016-05-17 09:03:38 -0400

    End to end testing part 2.

--- Angular-Fundamentals/DemoApp/test/e2e/scenarios.js
+++ Angular-Fundamentals/DemoApp/test/e2e/scenarios.js
@@ -34,6 +34,28 @@ describe('event registration app', function() {
             var list = element.all(by.repeater('session in event.sessions'));
             expect(list.count()).toEqual(3);
         });
+
+        it('should have 1 session when introductory is chosen', function() {
+            var selectEl = element(by.model('query.level'));
+            selectEl.element(by.cssContainingText('option', 'Introductory')).click();
+            var list = element.all(by.repeater('session in event.sessions'));
+            expect(list.count()).toEqual(1);
+        });
+
+        it('should sort correctly when sort order is changed', function(){
+            var selectEl = element(by.model('sortOrder'));
+            selectEl.element(by.cssContainingText('option', 'Votes')).click();
+            var firstSession = element.all(by.repeater('session in event.sessions')).first();
+            var firstSessionName = firstSession.element(by.binding('title')).getText();
+            expect(firstSessionName).toEqual('Scopes for fun and profit');
+
+        });
+
+        it('should increment the vote count when a session in upvoted', function() {
+            element.all(by.deepCss('div.votingButton')).first().click();
+            var firstVoteCount = element.all(by.binding('count')).first();
+            expect(firstVoteCount.getText()).toEqual('1');
+        });
    });
 
 });
